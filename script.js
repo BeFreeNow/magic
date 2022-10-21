8910e1710c9596ef5105e6165b1db3d1f23ca133
Igor S <befreenow3000@gmail.com>
2022-10-22 01:03:19 +0300

    add variables

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var body = document.querySelector('body');
+body.style.backgroundColor = getRandomColor();
 var mainCanvas = document.getElementById('myCanvas');
 var mainContext = mainCanvas.getContext('2d');
 var isPlaying = false;
@@ -7,6 +9,34 @@ var bufferSize = 4096;
 var soundLength = 15000;
 var audioContext;
 var brownNoise;
+
+var WHITE_FACTOR = getRandom(1, 3);
+const RED = getRandom(0, 255);
+const GREEN = getRandom(0, 255);
+const BLUE = getRandom(0, 255);
+const TEXT_COLOR = invertRgb(RED, GREEN, BLUE);
+const SIZE = getRandom(0, 100);
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function generateRandomColor(){
+  return `rgba(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0.01, 0.6)})`;
+}
+
+function invertRgb(r, g, b) {
+  return [255 - r, 255 - g, 255 - b];
+}
+
+
+
+
+
+function getRandomColor(maxOpacity = 0.6) {
+//get random rgba color
+  return `rgba(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0.01, 0.3)})`;
+}
 
 var requestAnimationFrame =
   window.requestAnimationFrame ||
@@ -24,7 +54,7 @@ function Circle(radius, speed, width, xPos, yPos) {
 
   this.counter = 0;
 
-  var signHelper = Math.floor(Math.random() * 3);
+  var signHelper = Math.floor(Math.random() * 5);
 
   if (signHelper == 1) {
     this.sign = -1;
@@ -37,13 +67,13 @@ let initialDate = new Date().toLocaleString();
 
 function personalize(userName = 'Vasili Ivanov', date = initialDate) {
   mainContext.font = '60px serif';
-  mainContext.fillStyle = 'lightgray';
+  mainContext.fillStyle = TEXT_COLOR;
   mainContext.fillText(`Personalized for ${userName}`, 100, 250);
   mainContext.font = '40px serif';
-  mainContext.fillStyle = 'white';
+  // mainContext.fillStyle = color;
   mainContext.fillText('WONT BE EFFECTIVE FOR OTHER VIEWERS!', 100, 375);
   mainContext.font = '50px serif';
-  mainContext.fillStyle = 'lightgray';
+  // mainContext.fillStyle = color;
   mainContext.fillText(`updated at ${date}`, 150, 450);
   mainContext.font = '50px serif';
   mainContext.fillText('*updated on every play, to prevent', 100, 600);
@@ -56,16 +86,16 @@ Circle.prototype.update = function () {
   mainContext.beginPath();
 
   mainContext.arc(
-    this.xPos + Math.cos(this.counter / 50) * this.radius,
-    this.yPos + Math.sin(this.counter / 50) * this.radius,
+    this.xPos + Math.cos(this.counter / 100) * this.radius,
+    this.yPos + Math.sin(this.counter / 100) * this.radius,
     this.width,
     0,
-    Math.PI * 2,
+    Math.PI * 5,
     false
   );
 
   mainContext.closePath();
-  mainContext.fillStyle = 'rgba(235, 245, 251,' + this.opacity + ')';
+  mainContext.fillStyle = `rgba(${RED}, ${GREEN}, ${BLUE}, ${this.opacity})`;
   mainContext.fill();
   personalize();
 };
@@ -77,10 +107,11 @@ function drawCircles() {
   for (var i = 0; i < 150; i++) {
     var randomX = Math.round(-100 + Math.random() * 1200);
     var randomY = Math.round(-100 + Math.random() * 1200);
-    var speed = 1 + Math.random() * 5;
-    var size = 1 + Math.random() * 10;
+    var speed = 1 + Math.random() * SIZE;
+    var size = 1 + Math.random() * getRandom(0, 100);
+    var radius = Math.round(100 + Math.random() * 300);
 
-    var circle = new Circle(30, speed, size, randomX, randomY);
+    var circle = new Circle(radius, speed, size, randomX, randomY);
     circles.push(circle);
   }
 
@@ -109,7 +140,7 @@ function playBrownNoise() {
     node.onaudioprocess = function (e) {
       var output = e.outputBuffer.getChannelData(0);
       for (var i = 0; i < bufferSize; i++) {
-        var white = Math.random() * 2 - 0.5;
+        var white = Math.random() * WHITE_FACTOR - 0.5;
         output[i] = (lastOut + 0.02 * white) / 1.02;
         lastOut = output[i];
         output[i] *= 0.5; // (roughly) compensate for gain
@@ -127,8 +158,9 @@ function play() {
   if (isPlaying) {
     return;
   }
-  initialDate = new Date().toLocaleString();
+
   isPlaying = true;
+  initialDate = new Date().toLocaleString();
   playBrownNoise();
   drawCircles();
   timeOut = setTimeout(() => {
@@ -140,6 +172,7 @@ function stop() {
   if (!isPlaying) {
     return;
   }
+
   isPlaying = false;
   clearTimeout(timeOut);
   mainContext?.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
